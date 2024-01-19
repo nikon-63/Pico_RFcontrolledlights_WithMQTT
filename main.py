1f85f932b198b6a5c0978734ebc6e1918eae5375
Tom N <79413139+nikon-63@users.noreply.github.com>
2024-01-19 18:41:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,72 +1,100 @@
+import machine
+import ubinascii
+import time
 import network
 from mqtt.simple import MQTTClient
 from machine import Pin
 import rfdevice
 import config
-import time
 
-led = Pin('LED', Pin.OUT)
-led.off()
+WIFI_SSID = ''
+WIFI_PASSWORD = ''
+MQTT_BROKER = ''
+MQTT_USER = ''
+MQTT_PASSWORD = ''
+CLIENT_ID = ubinascii.hexlify(machine.unique_id())
+TOPICS = [b"MainBedLights/Right/Set", b"MainBedLights/Left/Set", b"MainBedLights/Main/Set"]
 
-
-def sendRF(Light, Stat):
-    # Off code then On code
-    right = [2395496960, 2177393152]
-    left = [2529714688, 2663932416]
-    if Light == "Right":
-        sender.tx_code(int(right[Stat]), int(2), int(710))
-    if Light == "Left":
-        sender.tx_code(int(left[Stat]), int(2), int(710))
-
-
-
-def sub_cb(topic, msg):
-    msg = msg.decode('utf-8')
-    print(topic)
-    if msg == "true":
-        led.on()
-        if topic == b'Bedsidelight/Right/Set':
-            #Right bed side light on
-            print("Right bed side light on")
-            sendRF("Right", 0)
-        if topic == b'Bedsidelight/Left/Set':
-            #Left bed side light on
-            sendRF("Left", 0)
-            print("Left bed side light on")
-    elif msg == "false":
-        led.off()
-        if topic == b'Bedsidelight/Right/Set':
-            #Right bed side light off
-            sendRF("Right", 1)
-            print("Right bed side light off")
-        if topic == b'Bedsidelight/Left/Set':
-            #Left bed side light off
-            sendRF("Left", 1)
-            print("Left bed side light off")
-
-
-#Connect WiFi
-wlan = network.WLAN(network.STA_IF)
-wlan.active(True)
-wlan.connect("<Wifi SSID>","<WiFi Password>")
-wlan.ifconfig(('192.168.100.41', '255.255.255.0', '192.168.100.1', '8.8.8.8'))
-time.sleep(5)
-
-#MQTT Settings and connection
-mqtt_server = '<MQTT IP>'
-client_id = 'PicoW'
-user_t = '<MQTT USERNAME>'
-password_t = '<MQTT PASSWORD>'
-client = MQTTClient(client_id, mqtt_server, user=user_t, password=password_t, keepalive=60)
-client.set_callback(sub_cb)
-client.connect()
-
+# RF code
+Left = [2328388096, 2194170368]
+Right = [2731041280, 2898813440]
+Main = [2462605824, 2630377984]
 sender = rfdevice.RFDevice()
 sender.enable_tx()
 
-while True:
-    # The MQTT topics 
-    client.subscribe("Bedsidelight/Right/Set")
-    client.subscribe("Bedsidelight/Left/Set")
-    time.sleep(1)
+
+def connect_wifi(ssid, password):
+    wlan = network.WLAN(network.STA_IF)
+    wlan.active(True)
+    if not wlan.isconnected():
+        print('Connecting to network...')
+        wlan.connect(ssid, password)
+        wlan.ifconfig(('192.168.100.41', '255.255.255.0', '192.168.100.1', '8.8.8.8'))
+        while not wlan.isconnected():
+            pass
+    print('Network Config:', wlan.ifconfig())
+
+
+def mqtt_callback(topic, msg):
+    if topic == b"MainBedLights/Right/Set":
+        if msg == b"0":
+            sender.tx_code(int(Right[0]), int(2), int(707))
+            sender.tx_code(int(Right[0]), int(2), int(710))
+            print('Right light turned OFF')
+        elif msg == b"1":
+            sender.tx_code(int(Right[1]), int(2), int(707))
+            sender.tx_code(int(Right[1]), int(2), int(710))
+            print('Right light turned ON')
+            
+    elif topic == b"MainBedLights/Left/Set":
+        if msg == b"0":
+            sender.tx_code(int(Left[0]), int(2), int(707))
+            sender.tx_code(int(Left[0]), int(2), int(710))
+            print('Left light turned OFF')
+        elif msg == b"1":
+            sender.tx_code(int(Left[1]), int(2), int(707))
+            sender.tx_code(int(Left[1]), int(2), int(710))
+            print('Left light turned ON')
+            
+    elif topic == b"MainBedLights/Main/Set":
+        if msg == b"0":
+            sender.tx_code(int(Main[0]), int(2), int(707))
+            sender.tx_code(int(Main[0]), int(2), int(710))
+            print('Main light turned OFF')
+        elif msg == b"1":
+            sender.tx_code(int(Main[1]), int(2), int(707))
+            sender.tx_code(int(Main[1]), int(2), int(710))
+            print('Main light turned ON')
+
+
+def connect_mqtt():
+    client = MQTTClient(CLIENT_ID, MQTT_BROKER, user=MQTT_USER, password=MQTT_PASSWORD)
+    client.set_callback(mqtt_callback)
+    client.connect()
+    print('Connected to %s MQTT broker' % MQTT_BROKER)
+    return client
+
+
+def main():
+    led = Pin('LED', Pin.OUT)
+    led.off()
+    connect_wifi(WIFI_SSID, WIFI_PASSWORD)
+    client = connect_mqtt()
+    led.on()
+    for topic in TOPICS:
+        client.subscribe(topic)
+        print('Subscribed %s Topic' % topic.decode())
+
+    try:
+        while True:
+            client.wait_msg()
+    except OSError as e:
+        print('Reading Error: Reconnecting...')
+        led.off()
+        time.sleep(5.0)
+        machine.reset()
+
+if __name__ == "__main__":
+    main()
+
 
